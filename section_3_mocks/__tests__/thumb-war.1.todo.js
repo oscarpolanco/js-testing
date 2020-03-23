ff41a636ce3bcdbc4a096d542b053ddd7bf2aa57
Oscar Polanco <opolanco@intellisys.com.do>
2020-03-23 16:53:43 -0400

    Use spyOn from jest to mock the function

--- section_3_mocks/__tests__/thumb-war.1.todo.js
+++ section_3_mocks/__tests__/thumb-war.1.todo.js
@@ -1,36 +1,29 @@
-// improved assertions for mocks
+// using jest utilities
 import thumbWar from '../thumb-war'
 import * as utils from '../utils'
 
 test('returns winner', () => {
-  const originalGetWinner = utils.getWinner
-  // change the getWinner implementation to a function
-  // that keeps track of how often it's called and
-  // the arguments it's called with (Hint #1)
-  utils.getWinner = (...args) => {
-    utils.getWinner.mock.calls.push(args);
-    return args[1];
-  };
-  utils.getWinner.mock = { calls: [] };
+  // replace these lines with a call to jest.spyOn and
+  // call to mockImplementation on the mocked function (See hint #1)
+  jest.spyOn(utils, 'getWinner');
+  utils.getWinner.mockImplementation((p1, p2) => p2);
 
   const winner = thumbWar('Ken Wheeler', 'Kent C. Dodds');
   expect(winner).toBe('Kent C. Dodds');
-  // add an assertion for how many times the getWinner function
-  // was supposed to be called (2 times) (Hint #2)
-  //
-  // add another assertion that every time it was called
-  // it was called with the right arguments: 'Ken Wheeler', 'Kent C. Dodds'
-  // (Hint #3)
   expect(utils.getWinner.mock.calls).toEqual([
     ['Ken Wheeler', 'Kent C. Dodds'],
     ['Ken Wheeler', 'Kent C. Dodds']
   ]);
 
-  utils.getWinner = originalGetWinner;
+
+
+  // replace the next two lines with a restoration of the original function
+  // (See hint #2)
+  utils.getWinner.mockRestore();
 })
 
 /*
-Hints below:
+Hints below
 
 
 
@@ -106,15 +99,17 @@ Hints below:
 
 Hint #1:
 
-There's nothing too magical going on here, you just need some place to store the state for every time the function is
-called. Something like this should do just fine (Sorry, this is the solution I have. I couldn't think of a way to hint
-at it without totally giving it away or leading you astray):
+Here's an example of those APIs:
 
-utils.getWinner = (...args) => {
-  utils.getWinner.mock.calls.push(args)
-  return args[1]
-}
-utils.getWinner.mock = {calls: []}
+const myObject = {foo: () => 'bar'}
+jest.spyOn(myObject, 'foo')
+myObject.foo.mockImplementation(() => 'not-bar')
+myObject.foo() === 'not-bar' // true
+
+
+See the solution file for the solution
+
+
 
 
 
@@ -123,31 +118,13 @@ utils.getWinner.mock = {calls: []}
 
 
 Hint #2:
-Depending on how you store the state of how many times it's been called, you might either do this:
 
-expect(timesCalled).toBe(2)
+If we wanted to restore the mocked `myObject.foo` function
+to its original implementation, we could do:
+myObject.foo.mockRestore()
 
-Or you might do this:
-
-expect(utils.getWinner.mock.calls).toHaveLength(2)
-
-Either way is fine.
-
-
-
-
-
-
-Hint #3:
-
-You can have assertions within a forEach and that's not entirely uncommon. Depending on how you're storing the state
-of the arguments its called with you might do this (#spoileralert this is the solution... sorry):
-
-utils.getWinner.mock.calls.forEach(args => {
-  expect(args).toEqual(['Ken Wheeler', 'Kent C. Dodds'])
-})
-
-
+And then the original implementation will be called.
+myObject.foo() === 'bar' // true
 
 
  */
