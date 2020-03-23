d2746e74e568a8f042a267fb17dbde7765e65d71
Oscar Polanco <opolanco@intellisys.com.do>
2020-03-23 17:26:30 -0400

    Use jest to choose the mock function that is on the mock directory

--- section_3_mocks/__tests__/thumb-war.1.todo.js
+++ section_3_mocks/__tests__/thumb-war.1.todo.js
@@ -1,47 +1,24 @@
-// avoid monkey-patching with jest.mock
+// use the jest __mocks__ directory
 import thumbWar from '../thumb-war'
-import * as utils from '../utils'
+import * as utilsMock from '../utils'
 
-// add an inline mock with the jest.mock API
-//
-// jest.mock(
-//   relativePathToModuleToMock,
-//   functionThatReturnsMockObject
-// )
-//
-// (Hint #1)
-jest.mock('../utils', () => {
-  const actualUtils = require.requireActual('../utils')
-  return {
-    ...actualUtils,
-    getWinner: jest.fn((p1, p2) => p2)
-  }
-});
-
-beforeEach(() => {
-  utils.getWinner.mockClear();
-});
+// remove the inline mock function and jest
+// will use the one that exists in the
+// __mocks__ directory which I created for you
+// already (you're welcome)
+jest.mock('../utils');
 
 test('returns winner', () => {
   const winner = thumbWar('Ken Wheeler', 'Kent C. Dodds');
   expect(winner).toBe('Kent C. Dodds');
-  expect(utils.getWinner).toHaveBeenCalledTimes(2);
-  utils.getWinner.mock.calls.forEach(args => {
+  expect(utilsMock.getWinner).toHaveBeenCalledTimes(2);
+  utilsMock.getWinner.mock.calls.forEach(args => {
     expect(args).toEqual(['Ken Wheeler', 'Kent C. Dodds'])
   });
-});
-
-test('returns winner again', () => {
-  const winner = thumbWar('Ken Wheeler', 'Kent C. Dodds');
-  expect(winner).toBe('Kent C. Dodds');
-  expect(utils.getWinner).toHaveBeenCalledTimes(2);
-  utils.getWinner.mock.calls.forEach(args => {
-    expect(args).toEqual(['Ken Wheeler', 'Kent C. Dodds'])
-  });
-});
+})
 
 /*
-Hint below:
+Hint below
 
 
 
@@ -88,11 +65,18 @@ Hint below:
 
 
 
-jest.mock('../utils', () => {
-  return {
-    // ...
-    // see answer in the solution file
-  }
-})
+
+
+
+
+
+
+
+Hint #1:
+
+jest.mock(relativePathToModuleToMock)
+
+
+
 
  */
